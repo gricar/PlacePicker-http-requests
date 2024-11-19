47122895c548f12b290fd8ee9f7c38a2c659e72e
Ricardo, Gabriel <gabriel.ricardo@boeing.com>
2024-11-19 17:11:34 -0300

    added places

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AvailablePlacesComponent } from './places/available-places/available-places.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [AvailablePlacesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
